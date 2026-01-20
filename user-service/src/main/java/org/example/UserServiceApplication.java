cbccf2550e0aac1160e43b8fa8f4e6ea53319945
cathleeb <cathleeb@student.21-school.ru>
2026-01-20 20:41:47 +0700

    restructuring

--- user-service/src/main/java/org/example/UserServiceApplication.java
+++ user-service/src/main/java/org/example/UserServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntensiveTasksProjectApplication {
+public class UserServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(IntensiveTasksProjectApplication.class, args);
+        SpringApplication.run(UserServiceApplication.class, args);
     }
 }
